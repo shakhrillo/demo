84362c895064316ea03ba7f6630cc8b43377a9f5
amirbek <amirbekazimov7@gmail.com>
2023-10-26 10:49:02 +0500

    login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,10 +1,57 @@
-import { IonContent, IonPage } from '@ionic/react';
+import {
+  IonContent,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonThumbnail,
+  IonItem,
+  IonText,
+  IonCard,
+  IonInput,
+  IonCheckbox,
+  IonButton,
+  IonButtons,
+} from '@ionic/react';
+import './login.css';
 
-export default function(): JSX.Element {
+export default function (): JSX.Element {
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
       <IonContent>
-        <h1>Login</h1>
+        <IonCard className='container-login'>
+          <IonThumbnail>
+            <img src='https://ionicframework.com/docs/demos/api/card/madison.jpg' />
+          </IonThumbnail>
+          <IonText class='head-text'>Lorem, ipsum dolor.</IonText>
+          <IonInput placeholder='Enter username' />
+          <IonInput type='password' placeholder='Enter password' />
+          <IonCheckbox labelPlacement='end'>
+            I agree to the terms and conditions
+          </IonCheckbox>
+          <IonButton>Login</IonButton>
+          <br />
+          <IonText>
+            Lorem ipsum dolor sit amet consectetur adipisicing Lorem, ipsum
+            dolor..
+          </IonText>
+          <br />
+          <IonItem>
+            <IonButton>start</IonButton>
+            <IonButton>center</IonButton>
+            <IonButton>end</IonButton>
+          </IonItem>
+          <IonText>
+            Lorem ipsum dolor sit amet consectetur adipisicing Lorem, ipsum
+            dolor..
+          </IonText>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
