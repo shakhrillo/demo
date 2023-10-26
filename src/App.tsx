bed04a29465e5ba484dfe40a783a0375489a9882
amirbek <amirbekazimov7@gmail.com>
2023-10-26 16:29:09 +0500

    added new-pasword page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
   IonIcon,
@@ -8,43 +8,42 @@ import {
   IonTabButton,
   IonTabs,
   setupIonicReact,
-} from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { ellipse, square, triangle } from "ionicons/icons";
-import Tab1 from "./pages/Tab1";
-import Tab2 from "./pages/Tab2";
-import Tab3 from "./pages/Tab3";
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import "./theme/variables.css";
-import Splash from "./pages/onboarding/splash";
-import Welcome from "./pages/onboarding/welcome";
-import Walkthrough from "./pages/onboarding/walkthrough";
-import Register from "./pages/auth/register";
-import Login from "./pages/auth/login";
-import ForgotPassword from "./pages/auth/forgot-password";
-import Countries from "./pages/account/countries";
-import Preference from "./pages/account/preference";
-import Settings from "./pages/account/settings";
-import Expertise from "./pages/account/expertise";
-import Filter from "./pages/account/filter";
-import Main from './pages/Home/main'
+import './theme/variables.css';
+import Splash from './pages/onboarding/splash';
+import Welcome from './pages/onboarding/welcome';
+import Walkthrough from './pages/onboarding/walkthrough';
+import Register from './pages/auth/register';
+import Login from './pages/auth/login';
+import ForgotPassword from './pages/auth/forgot-password';
+import Countries from './pages/account/countries';
+import Preference from './pages/account/preference';
+import Settings from './pages/account/settings';
+import Expertise from './pages/account/expertise';
+import NewPassword from './pages/auth/new-password';
 
 setupIonicReact();
 
@@ -53,63 +52,62 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
+          <Route exact path='/tab1'>
             <Tab1 />
           </Route>
-          <Route exact path="/tab1">
-              <Tab1 />
-            </Route>
-            <Route exact path='/tab2'>
-              <Tab2 />
-            </Route>
-            <Route path='/tab3'>
-              <Tab3 />
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/tab1" />
-            </Route>
+          <Route exact path='/tab1'>
+            <Tab1 />
+          </Route>
+          <Route exact path='/tab2'>
+            <Tab2 />
+          </Route>
+          <Route path='/tab3'>
+            <Tab3 />
+          </Route>
+          <Route exact path='/'>
+            <Redirect to='/tab1' />
+          </Route>
           {/* Pages */}
-          <Route exact path="/">
+          <Route exact path='/'>
+            <Redirect to='/tab1' />
+          </Route>
+
+          {/* Pages */}
+          <Route exact path='/'>
             <Splash />
           </Route>
-          <Route exact path="/settings">
+          <Route exact path='/settings'>
             <Settings />
           </Route>
-          <Route exact path="/welcome">
+          <Route exact path='/new-password'>
+            <NewPassword />
+          </Route>
+          <Route exact path='/welcome'>
             <Welcome />
           </Route>
-          <Route exact path="/walkthrough">
+          <Route exact path='/walkthrough'>
             <Walkthrough />
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/forgot-password">
-            <ForgotPassword />
-          </Route>
-          <Route exact path="/countries">
-            <Countries />
-          </Route>
-          <Route exact path="/preference">
-            <Preference />
-          </Route>
-          <Route exact path="/expertise">
-            <Expertise />
-          </Route>
-          <Route exact path="/filter">
-            <Filter />
-          </Route>
-          <Route exact path="/main">
-            <Main />
           </Route>
           <Route exact path='/register'>
             <Register />
           </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/forgot-password'>
+            <ForgotPassword />
+          </Route>
+          <Route exact path='/countries'>
+            <Countries />
+          </Route>
+          <Route exact path='/preference'>
+            <Preference />
+          </Route>
+          <Route exact path='/expertise'>
+            <Expertise />
+          </Route>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
+        {/* <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
@@ -122,7 +120,7 @@ const App: React.FC = () => (
             <IonIcon aria-hidden='true' icon={square} />
             <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
-        </IonTabBar>
+        </IonTabBar> */}
       </IonTabs>
     </IonReactRouter>
   </IonApp>
