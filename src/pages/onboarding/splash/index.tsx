3d1aaa5ba9d8991eeb4ba08361cc2daf4ad0db31
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2023-10-26 11:43:28 +0500

    Updated changes

--- src/pages/onboarding/splash/index.tsx
+++ src/pages/onboarding/splash/index.tsx
@@ -1,25 +1,20 @@
-import { IonContent, IonPage, IonImg, IonSpinner, IonRow, IonCol, IonGrid } from '@ionic/react';
-
+import { IonPage, IonImg, IonSpinner, IonRow, IonCol, IonGrid } from '@ionic/react';
+import imgLogo from '../../../assets/images/logo.jpeg';
 
 export default function(): JSX.Element {
   return (
-    <IonPage className="page">
-      <IonGrid class='grid' style={{padding:50}}>
+    <IonPage>
+      <IonGrid class='container'>
         <IonRow>
           <IonCol>
             <IonImg
-              src="https://docs-demo.ionic.io/assets/madison.jpg"
+              src={ imgLogo }
+              style={{ width: 200 }}
               alt="The Wisconsin State Capitol building in Madison, WI at night">
             </IonImg>
           </IonCol>
         </IonRow>
-      </IonGrid>
-      <IonGrid>
-      </IonGrid>
-      <IonGrid>
-      </IonGrid>
-      <IonGrid>
-      <IonRow>
+        <IonRow style={{height: 40, marginTop: 'auto', marginBottom: 60}}>
           <IonCol>
             <IonSpinner></IonSpinner>
           </IonCol>
--- src/pages/onboarding/welcome/index.tsx
+++ src/pages/onboarding/welcome/index.tsx
@@ -3,31 +3,19 @@ import { IonGrid, IonPage, IonText, IonRow, IonCol } from "@ionic/react";
 export default function (): JSX.Element {
   return (
     <IonPage>
-      <IonGrid>
-        <IonRow>
-          <IonCol></IonCol>
-        </IonRow>
-      </IonGrid>
-      <IonGrid>
-        <IonRow>
-          <IonCol></IonCol>
-        </IonRow>
-      </IonGrid>
-      <IonGrid>
-        <IonRow>
-          <IonCol></IonCol>
-        </IonRow>
-      </IonGrid>
-
-      <IonGrid>
-        <IonRow>
+      <IonGrid style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+        <IonRow style={{ marginTop: 'auto' }}>
           <IonCol>
-            <IonText>Lorem ipsum dolor sit.</IonText>
+            <IonText>
+              <h1>Welcome to Jobee!</h1>
+            </IonText>
           </IonCol>
         </IonRow>
         <IonRow>
           <IonCol>
-            <IonText>Lorem ipsum dolor sit.</IonText>
+            <IonText>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate cupiditate non
+            </IonText>
           </IonCol>
         </IonRow>
       </IonGrid>
