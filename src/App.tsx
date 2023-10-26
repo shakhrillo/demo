ddf6a7b54e824babc698dfa6bb5d35d3b263ecc3
amirbek <amirbekazimov7@gmail.com>
2023-10-26 11:57:08 +0500

    login page with grids

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
+  setupIonicReact,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
@@ -47,52 +47,50 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
+          <Route exact path='/tab1'>
             <Tab1 />
           </Route>
-          <Route exact path="/tab2">
+          <Route exact path='/tab2'>
             <Tab2 />
           </Route>
-          <Route path="/tab3">
+          <Route path='/tab3'>
             <Tab3 />
           </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
+          <Route exact path='/'>
+            <Redirect to='/tab1' />
           </Route>
-
 
           {/* Pages */}
-          <Route exact path="/splash">
+          <Route exact path='/splash'>
             <Splash />
           </Route>
-          <Route exact path="/welcome">
+          <Route exact path='/welcome'>
             <Welcome />
           </Route>
-          <Route exact path="/walkthrough">
+          <Route exact path='/walkthrough'>
             <Walkthrough />
           </Route>
-          <Route exact path="/register">
+          <Route exact path='/register'>
             <Register />
           </Route>
-          <Route exact path="/login">
+          <Route exact path='/login'>
             <Login />
           </Route>
-          <Route exact path="/forgot-password">
+          <Route exact path='/forgot-password'>
             <ForgotPassword />
           </Route>
-
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
+        <IonTabBar slot='bottom'>
+          <IonTabButton tab='tab1' href='/tab1'>
+            <IonIcon aria-hidden='true' icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
+          <IonTabButton tab='tab2' href='/tab2'>
+            <IonIcon aria-hidden='true' icon={ellipse} />
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
+          <IonTabButton tab='tab3' href='/tab3'>
+            <IonIcon aria-hidden='true' icon={square} />
             <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
         </IonTabBar>
--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -11,9 +11,11 @@ import {
   IonInput,
   IonCheckbox,
   IonButton,
-  IonButtons,
+  IonCol,
+  IonGrid,
+  IonRow,
 } from '@ionic/react';
-import './login.css';
+import './index.css';
 
 export default function (): JSX.Element {
   return (
@@ -25,33 +27,49 @@ export default function (): JSX.Element {
       </IonHeader>
 
       <IonContent>
-        <IonCard className='container-login'>
-          <IonThumbnail>
-            <img src='https://ionicframework.com/docs/demos/api/card/madison.jpg' />
-          </IonThumbnail>
-          <IonText class='head-text'>Lorem, ipsum dolor.</IonText>
-          <IonInput placeholder='Enter username' />
-          <IonInput type='password' placeholder='Enter password' />
-          <IonCheckbox labelPlacement='end'>
-            I agree to the terms and conditions
-          </IonCheckbox>
-          <IonButton>Login</IonButton>
-          <br />
-          <IonText>
-            Lorem ipsum dolor sit amet consectetur adipisicing Lorem, ipsum
-            dolor..
-          </IonText>
-          <br />
-          <IonItem>
-            <IonButton>start</IonButton>
-            <IonButton>center</IonButton>
-            <IonButton>end</IonButton>
-          </IonItem>
-          <IonText>
-            Lorem ipsum dolor sit amet consectetur adipisicing Lorem, ipsum
-            dolor..
-          </IonText>
-        </IonCard>
+        <IonGrid style={{ padding: 0 }}>
+          <IonRow>
+            <IonCol class='login-container'>
+              <IonThumbnail>
+                <img src='https://ionicframework.com/docs/demos/api/card/madison.jpg' />
+              </IonThumbnail>
+              <IonText class='head-text'>Lorem, ipsum dolor.</IonText>
+              <IonInput placeholder='Enter username' />
+              <IonInput type='password' placeholder='Enter password' />
+              <IonCheckbox labelPlacement='end'>
+                I agree to the terms and conditions
+              </IonCheckbox>
+              <IonButton>Login</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+          <IonRow>
+            <IonCol></IonCol>
+          </IonRow>
+        </IonGrid>
+        <br />
+        <IonGrid>
+          <IonRow>
+            <IonCol class='login-container'>
+              <IonText>
+                Lorem ipsum dolor sit amet consectetur adipisicing Lorem, ipsum
+                dolor..
+              </IonText>
+              <br />
+              <IonItem>
+                <IonButton>start</IonButton>
+                <IonButton>center</IonButton>
+                <IonButton>end</IonButton>
+              </IonItem>
+              <IonText>
+                Lorem ipsum dolor sit amet consectetur adipisicing Lorem, ipsum
+                dolor..
+              </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
