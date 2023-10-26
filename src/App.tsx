e719a1fccb4c65b082d7ff6f3a749202cac60a9c
amirbek <amirbekazimov7@gmail.com>
2023-10-26 15:44:20 +0500

    added settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
   IonIcon,
@@ -8,40 +8,41 @@ import {
   IonTabButton,
   IonTabs,
   setupIonicReact,
-} from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { ellipse, square, triangle } from "ionicons/icons";
-import Tab1 from "./pages/Tab1";
-import Tab2 from "./pages/Tab2";
-import Tab3 from "./pages/Tab3";
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { ellipse, square, triangle } from 'ionicons/icons';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import "./theme/variables.css";
-import Splash from "./pages/onboarding/splash";
-import Welcome from "./pages/onboarding/welcome";
-import Walkthrough from "./pages/onboarding/walkthrough";
-import Register from "./pages/auth/register";
-import Login from "./pages/auth/login";
-import ForgotPassword from "./pages/auth/forgot-password";
-import Countries from "./pages/account/countries";
-import Preference from "./pages/account/preference";
-import Expertise from "./pages/account/expertise";
+import './theme/variables.css';
+import Splash from './pages/onboarding/splash';
+import Welcome from './pages/onboarding/welcome';
+import Walkthrough from './pages/onboarding/walkthrough';
+import Register from './pages/auth/register';
+import Login from './pages/auth/login';
+import ForgotPassword from './pages/auth/forgot-password';
+import Countries from './pages/account/countries';
+import Preference from './pages/account/preference';
+import Settings from './pages/account/settings';
+import Expertise from './pages/account/expertise';
 
 setupIonicReact();
 
@@ -64,31 +65,34 @@ const App: React.FC = () => (
           </Route> */}
 
         {/* Pages */}
-        <Route exact path="/">
+        <Route exact path='/'>
           <Splash />
         </Route>
-        <Route exact path="/welcome">
+        <Route exact path='/settings'>
+          <Settings />
+        </Route>
+        <Route exact path='/welcome'>
           <Welcome />
         </Route>
-        <Route exact path="/walkthrough">
+        <Route exact path='/walkthrough'>
           <Walkthrough />
         </Route>
-        <Route exact path="/register">
+        <Route exact path='/register'>
           <Register />
         </Route>
-        <Route exact path="/login">
+        <Route exact path='/login'>
           <Login />
         </Route>
-        <Route exact path="/forgot-password">
+        <Route exact path='/forgot-password'>
           <ForgotPassword />
         </Route>
-        <Route exact path="/countries">
+        <Route exact path='/countries'>
           <Countries />
         </Route>
-        <Route exact path="/preference">
+        <Route exact path='/preference'>
           <Preference />
         </Route>
-        <Route exact path="/expertise">
+        <Route exact path='/expertise'>
           <Expertise />
         </Route>
       </IonRouterOutlet>
---
+++ src/pages/account/settings/index.tsx
@@ -0,0 +1,100 @@
+import {
+  IonContent,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonCol,
+  IonGrid,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/react';
+import '../../auth/login/index.css';
+import userLogo from '../../../assets/images/user-logo.jpeg';
+import logo from '../../../assets/images/logo.jpeg';
+
+export default function (): JSX.Element {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>
+        <IonGrid style={{ padding: 10 }}>
+          <IonRow style={{ width: 100, margin: 'auto', position: 'relative' }}>
+            <IonCol
+              style={{
+                background: `url(${userLogo})`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+                height: '120px',
+                width: '100px',
+                borderRadius: '50%',
+              }}
+            ></IonCol>
+            <div
+              style={{
+                background: 'blue',
+                width: '25px',
+                height: '25px',
+                position: 'absolute',
+                borderRadius: 10,
+                bottom: 0,
+                right: 0,
+              }}
+            ></div>
+          </IonRow>
+
+          <IonRow>
+            <IonCol class='login-container settings'>
+              <IonInput
+                placeholder='Full Name'
+                type='text'
+                class='custom'
+              ></IonInput>
+              <IonInput
+                placeholder='Nickname'
+                type='password'
+                class='custom'
+              ></IonInput>
+              <IonInput
+                placeholder='Date of Birth'
+                type='password'
+                class='custom'
+              ></IonInput>
+              <IonInput
+                placeholder='Email'
+                type='email'
+                class='custom'
+              ></IonInput>
+              <IonInput
+                placeholder='+1 234 567 8900'
+                type='number'
+                class='custom'
+              ></IonInput>
+              <IonSelect class='custom' label='Gender'>
+                <IonSelectOption value='apple'>Male</IonSelectOption>
+                <IonSelectOption value='banana'>Female</IonSelectOption>
+              </IonSelect>
+              <IonButton
+                color='tertiary'
+                style={{ width: '100%' }}
+                shape='round'
+                expand='full'
+              >
+                Continue
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+}
