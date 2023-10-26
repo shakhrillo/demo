fd272df0ab28d7c49e2f5f1f49235faf0316c20b
Jobir <jobirashurov7877@gmail.com>
2023-10-26 11:21:07 +0500

    welcome edited

--- src/pages/onboarding/welcome/index.tsx
+++ src/pages/onboarding/welcome/index.tsx
@@ -1,16 +1,36 @@
-import { IonCard, IonContent, IonItem, IonPage, IonText } from "@ionic/react";
-import "./index.css";
+import { IonGrid, IonPage, IonText, IonRow, IonCol } from "@ionic/react";
 
 export default function (): JSX.Element {
   return (
     <IonPage>
-      <IonContent class="background-image content">
-        <IonCard className="content">
-          <IonText className="large-text">Lorem, ipsum dolor.</IonText>
-          <br />
-          <IonText className="">Lorem, ipsum dolor.</IonText>
-        </IonCard>
-      </IonContent>
+      <IonGrid>
+        <IonRow>
+          <IonCol></IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonGrid>
+        <IonRow>
+          <IonCol></IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonGrid>
+        <IonRow>
+          <IonCol></IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonText>Lorem ipsum dolor sit.</IonText>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonText>Lorem ipsum dolor sit.</IonText>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </IonPage>
   );
 }
