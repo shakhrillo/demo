10fcbaa7a867397d0a0b90777de9ebe36183e081
Jobir <jobirashurov7877@gmail.com>
2023-10-27 16:32:57 +0500

    welcome style

--- src/pages/onboarding/welcome/index.tsx
+++ src/pages/onboarding/welcome/index.tsx
@@ -1,4 +1,12 @@
-import { IonGrid, IonPage, IonText, IonRow, IonCol, IonButton } from "@ionic/react";
+import {
+  IonGrid,
+  IonPage,
+  IonText,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonContent,
+} from "@ionic/react";
 import { useEffect } from "react";
 import { useHistory } from "react-router";
 import "./index.css";
@@ -7,33 +15,35 @@ export default function (): JSX.Element {
   const history = useHistory();
 
   const navigate = () => {
-    history.replace('walkthrough')
-  }
+    history.replace("walkthrough");
+  };
 
   return (
     <IonPage>
-      <IonGrid onClick={navigate} className="welcome-cotainer">
-        <IonRow style={{ marginTop: "auto" }}>
-          <IonCol>
-            <IonText>
-              <h1>
-                Welcome to <br />
-                Jobee!👋
-              </h1>
-            </IonText>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonText>
-              <p>
-                The best job finder & job portal app where <br /> the best jobs
-                will find you
-              </p>
-            </IonText>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
+      <IonContent>
+        <IonGrid onClick={navigate} className="welcome-cotainer">
+          <IonRow style={{ marginTop: "auto" }}>
+            <IonCol>
+              <IonText>
+                <h1>
+                  Welcome to <br />
+                  Jobee!👋
+                </h1>
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonText>
+                <p>
+                  The best job finder & job portal app where <br /> the best
+                  jobs will find you
+                </p>
+              </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonPage>
   );
 }
