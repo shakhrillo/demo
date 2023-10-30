577a863291dfdf9143a1087e27070bff9a3105c1
Mukhammad <muxammadhotamo@gmail.com>
2023-10-30 12:17:28 +0500

    new changes

---
+++ src/components/walkthrough/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { IonContent, IonImg } from "@ionic/react";
+
+import "./index.css"
+
+export default function (props: {url:string, children: any}): JSX.Element {
+    const {width, height} = window.screen
+    return(
+        <>
+            <IonContent style={{width, height, "--background": `url(${props.url}) no-repeat center center / cover`, positoon: "absolute", zIndex: -1}}>
+                {props.children}
+            </IonContent>
+        </>
+    )
+}
