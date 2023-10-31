9489e37b0040c7302d3c2b5e95b7817466a1bf94
naimovdavlatshoh <davlatshoh.fullstack@gmail.com>
2023-10-31 12:07:37 +0500

    code verification

--- src/views/verification/index.tsx
+++ src/views/verification/index.tsx
@@ -1,14 +1,77 @@
 import { IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonList, IonPage, IonRow, IonSelect, IonSelectOption, IonText, IonTitle, IonToolbar } from "@ionic/react";
 import React from "react";
+import { useRef, useState, useEffect } from "react";
 
-export const Verification: React.FC = () => (
-  <IonPage>
+export const Verification: React.FC = () => {
+  const [firstInput, setFirstInput] = useState<any>("")
+  const [secondInput, setSecondInput] = useState("")
+  const [thirdInput, setThirdInput] = useState("")
+  const [fourthInput, setFourthInput] = useState("")
+  const [active, setActive] = useState(false)
+  const [counter, setCounter] = useState(5)
+
+  const firstInputRef = useRef(null)
+  const secondInputRef = useRef(null)
+  const thirdInputRef = useRef(null)
+  const fourthInputRef = useRef(null)
+
+  const timer = () =>{
+    if(active){
+      setCounter(counter-1)
+    }
+  }
+
+  const changeActive = () => {
+    setActive(false)
+  }
+  
+  setTimeout(timer, 1000)
+
+  const setFalse = () => {
+    setActive(true)
+    setTimeout(changeActive, 5000)
+    setCounter(5)
+  }
+
+  const handleFirstInput = (event: any) => {
+    const firstval = event.target.value
+    setFirstInput(firstval)
+    if(firstval.length ===1){
+      secondInputRef.current.setFocus();
+    }
+  }
+  const handleSecondInput = (event: any) => {
+    const secondval = event.target.value
+    setSecondInput(secondval)
+    if(secondval.length ===1){
+      thirdInputRef.current.setFocus()
+    }
+  }
+  const handleThirdInput = (event: any) => {
+    const thirdval = event.target.value
+    setThirdInput(thirdval)
+    if(thirdval.length ===1){
+      fourthInputRef.current.setFocus()
+    }
+  }
+  const handleFourthInput = (event: any) => {
+    const fourthval = event.target.value
+    setFourthInput(thirdval)
+    if(fourthval.length ===1){
+      fourthInputRef.current.setFocus()
+    }
+  }
+
+  
+
+  return(
+    <IonPage>
     <IonHeader className="ion-no-border">
       <IonToolbar color="transparent">
         <IonButtons slot="start">
           <IonBackButton defaultHref="" color="primary"></IonBackButton>
         </IonButtons>
-        <IonButton slot="end" disabled>Resend Code</IonButton>
+        <IonButton  slot="end" onClick={setFalse} disabled={active}>Resend Code</IonButton>
       </IonToolbar>
     </IonHeader>
     <IonContent>
@@ -29,16 +92,21 @@ export const Verification: React.FC = () => (
         </IonRow>
         <IonRow class="ion-text-center">
           <IonCol>
-            <IonInput type="number" placeholder="0"></IonInput>
+            <IonInput onIonInput={handleFirstInput} ref={firstInputRef} inputMode={"numeric"} maxlength={1} counter={false} placeholder="0"></IonInput>
           </IonCol>
           <IonCol>
-            <IonInput type="number" placeholder="0"></IonInput>
+            <IonInput onIonInput={handleSecondInput} ref={secondInputRef} inputMode={"numeric"} value={secondInput} maxlength={1} counter={false} placeholder="0"></IonInput>
           </IonCol>
           <IonCol>
-            <IonInput type="number" placeholder="0"></IonInput>
+            <IonInput onIonInput={handleThirdInput} ref={thirdInputRef} inputMode={"numeric"} value={thirdInput} maxlength={1} counter={false} placeholder="0"></IonInput>
           </IonCol>
           <IonCol>
-            <IonInput type="number" placeholder="0"></IonInput>
+            <IonInput onIonInput={handleFourthInput} ref={fourthInputRef} inputMode={"numeric"} value={fourthInput} maxlength={1} counter={false} placeholder="0"></IonInput>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            {active?<h1>{counter}</h1>:""}
           </IonCol>
         </IonRow>
         <IonRow className="pu-margin-top-auto">
@@ -54,4 +122,5 @@ export const Verification: React.FC = () => (
       </IonGrid>
     </IonContent>
   </IonPage>
-);
+  )
+};
